980d0674822cc1bfeba277ef0eadd30a436fe3cf
bfigueroa-2020308 <89117542+bfigueroa-2020308@users.noreply.github.com>
2022-05-20 15:31:01 -0600

    Se actualizo el archivo app
    Se actualizo el archivo app agregando las rutas de los controladores

--- config/app.js
+++ config/app.js
@@ -4,6 +4,10 @@ const cors = require('express');
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const empresaRoutes = require('../src/routes/empresas.routes');
+const productoEmpresaRoutes = require('../src/routes/productoEmpresa.routes');
+const sucursalRoutes = require('../src/routes/sucursal.routes');
+const productoSucursalRoutes =require('../src/routes/productoSucursal.routes');
 
 const app = express();
 
@@ -11,5 +15,9 @@ app.use(helmet());
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use('/Empresa', empresaRoutes);
+app.use('/ProductoEmpresa', productoEmpresaRoutes);
+app.use('/Sucursal', sucursalRoutes);
+app.use('/ProductoSucursal', productoSucursalRoutes);
 
 module.exports = app;
